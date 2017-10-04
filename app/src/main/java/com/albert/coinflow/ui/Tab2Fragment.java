4253a81f400e884818567ee988d233f758d2a554
Okwonks <albertoketch@gmail.com>
2017-10-04 09:23:47 +0300

    Linked new fragments to the main activity.

---
+++ app/src/main/java/com/albert/coinflow/ui/Tab2Fragment.java
@@ -0,0 +1,19 @@
+package com.albert.coinflow.ui;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.albert.coinflow.R;
+
+public class Tab2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab2, container, false);
+        return rootView;
+    }
+}
