1332bfc47212542cc9d3456ccace2386d3c071bd
Okwonks <albertoketch@gmail.com>
2017-10-04 07:44:03 +0300

    Implementing FirebaseAuthListeners to the apps login activity.

--- app/src/main/java/com/albert/coinflow/ui/LoginActivity.java
+++ app/src/main/java/com/albert/coinflow/ui/LoginActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.albert.coinflow.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -50,11 +55,70 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
 
-        // Auth progress dialog goes here.
+        createAuthProgressDialog();
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this); // Deprecated value ProgressDialog
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    /* Method to handle login in with password */
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Sorry, something missing");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Didn't see a password!");
+            return;
+        }
+        mAuthProgressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            Log.w("Sign in", "signInWithEmail:onComplete" + task.isSuccessful());
+                            Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     @Override
     public void onClick(View v) {
+        if (v == mSignUpTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
+        if (v == mLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    /** Methods to handle the authentication listeners
+     * this will ensure the authentication listener runs
+     * for a specific time and will be stopped after */
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
