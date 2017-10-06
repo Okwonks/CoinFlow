c0e3f50fe3c82596d65459fb1a8c115781a81762
Okwonks <albertoketch@gmail.com>
2017-10-06 09:30:29 +0300

    Inserting a calendar view for the Monthly fragment.

--- app/src/main/java/com/albert/coinflow/ui/Tab3Fragment.java
+++ app/src/main/java/com/albert/coinflow/ui/Tab3Fragment.java
@@ -13,7 +13,7 @@ public class Tab3Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_tab3, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_monthly, container, false);
         return rootView;
     }
 }
